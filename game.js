fd6d2f808d500b78a3e361a26ae8c640626d8c29
Taras R <tarasromanets01@gmail.com>
2025-04-28 23:08:31 +0300

    Button click and leveling up

--- game.js
+++ game.js
@@ -1,19 +1,46 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false;
 
 function nextSequence() {
-    return Math.floor(Math.random() * 4);
+    level++;
+    $("#level-title").text("Level " + level);
+    
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
 }
 
-$(document).ready(function() {
-    $(document).keypress(function() {
-        let randomNumber = nextSequence();
-        let randomChosenColor = buttonColors[randomNumber];
-        gamePattern.push(randomChosenColor);
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
 
-        $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
 
-        var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-        audio.play();
-    });
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").on("click", function() {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    nextSequence(); // ? needed?
 });
