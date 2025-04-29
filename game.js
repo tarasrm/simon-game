ac48e1f8bedb5d8ce69c07b20d01e5ccc4f4d120
Taras R <tarasromanets01@gmail.com>
2025-04-29 21:32:32 +0300

    Check clicked buttons sequence logic

--- game.js
+++ game.js
@@ -4,10 +4,48 @@ let userClickedPattern = [];
 let level = 0;
 let started = false;
 
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").on("click", function() {
+    if (!started) return;
+    
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        started = false;
+        level = 0;
+        gamePattern = [];
+        userClickedPattern = [];
+    }
+}
+
 function nextSequence() {
+    userClickedPattern = [];
     level++;
     $("#level-title").text("Level " + level);
-    
+
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
@@ -27,22 +65,3 @@ function animatePress(currentColor) {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
 }
-
-$(document).keypress(function() {
-    if (!started) {
-        $("#level-title").text("Level " + level);
-        nextSequence();
-        started = true;
-    }
-});
-
-$(".btn").on("click", function() {
-    let userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-    playSound(userChosenColor);
-    animatePress(userChosenColor);
-
-    setTimeout(function() {
-        nextSequence();
-    }, 1000);
-});
